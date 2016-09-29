938c5d013c3b8887ec074bf8d59c0469ee6b34da
Shannon Ryan Dunn <shannonrdunn@gmail.com>
2016-09-28 22:14:07 -0700

    add parent

--- issue.go
+++ issue.go
@@ -83,6 +83,7 @@ type IssueFields struct {
 	Updated           string        `json:"updated,omitempty"`
 	Description       string        `json:"description,omitempty"`
 	Summary           string        `json:"summary"`
+	Parent		  string	`json:"parent"`
 	Creator           *User         `json:"Creator,omitempty"`
 	Reporter          *User         `json:"reporter,omitempty"`
 	Components        []*Component  `json:"components,omitempty"`
